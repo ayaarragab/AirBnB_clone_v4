ada2ec5cc23378af1de96e88784656a7989e48a0
ayaarragab <ayarsk4343@gmail.com>
2024-03-03 16:25:38 +0200

    2-hbnb

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,26 @@
+// $(document).ready(function() {
+//     const amenities = {};
+ 
+//     $('input[type="checkbox"]').change(function() {
+//        const amenityId = $(this).attr('data-id');
+//        const amenityName = $(this).attr('data-name');
+ 
+//        if ($(this).is(':checked')) {
+//           amenities[amenityId] = amenityName;
+//        } else {
+//           delete amenities[amenityId];
+//        }
+ 
+//        const amenitiesList = Object.values(amenities);
+//        $('div.Amenities h4').text(amenitiesList);
+//     });
+//  });
+$.get('http://localhost:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+        console.log(data.status);
+        $('#api_status').addClass('available');
+    } else {
+        $('#api_status').removeClass('available');
+    }
+    
+}) 
